b8988d9408eb32dfb64afa54e9d0949c7d662b00
a.silitski <a.siliski@sumatosoft.com>
2022-05-31 00:18:18 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create!  :email => 'admin@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', role: :admin
+user = User.create!  :email => 'adminn@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', role: :admin
 
