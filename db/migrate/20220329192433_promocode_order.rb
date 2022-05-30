f7e2da03b59a265afc4ce13ed9b0caaaef46ef52
a.silitski <a.siliski@sumatosoft.com>
2022-05-30 22:52:29 +0300

    fix db

--- db/migrate/20220329192433_promocode_order.rb
+++ db/migrate/20220329192433_promocode_order.rb
@@ -1,8 +1,8 @@
 class PromocodeOrder < ActiveRecord::Migration[6.1]
   def change
     create_join_table :order, :promocode do |t|
-      t.index :order
-      t.index :promocode
+      t.index [:order_id, :promocode_id]
+      t.index [:promocode_id, :order_id]
     end
   end
 end
