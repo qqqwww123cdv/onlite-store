b6c15525d9acd646880ac0b50c1eb56fccaa566d
a.silitski <a.siliski@sumatosoft.com>
2022-06-03 17:23:23 +0300

    for pushing

--- db/migrate/20220317191436_change_string_to_boolean_of_pickup.rb
+++ db/migrate/20220317191436_change_string_to_boolean_of_pickup.rb
@@ -1,5 +1,5 @@
 class ChangeStringToBooleanOfPickup < ActiveRecord::Migration[6.1]
   def change
-    change_column :checkouts, :pick_up, :boolean
+    change_column :checkouts, :pick_up, 'boolean USING CAST(pick_up AS boolean)'
   end
 end
